2491b9b54655fe7b299bb6cd75aaf2d0551ac00c
Christian Fei <crifei93@gmail.com>
2019-08-17 20:49:54 +0200

    fix carbonads and copy assets

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("style.css");
+  eleventyConfig.addPassthroughCopy("carbonads.css");
+  eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addCollection("posts", (collection) => {
     return collection.getFilteredByTag('post').reverse();
   });
