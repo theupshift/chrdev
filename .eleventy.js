5334754c828630f8b56dffc8cff28e6f3df727f7
Christian Fei <crifei93@gmail.com>
2019-08-17 20:57:51 +0200

    add robots.txt

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("style.css");
   eleventyConfig.addPassthroughCopy("carbonads.css");
   eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("robots.txt");
   eleventyConfig.addCollection("posts", (collection) => {
     return collection.getFilteredByTag('post').reverse();
   });
