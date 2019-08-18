d9933e27b87ff5810066b461bf20fe505942a8de
Christian Fei <crifei93@gmail.com>
2019-08-18 16:03:54 +0200

    add dynamic meta description with excerpt for postsimage

--- .eleventy.js
+++ .eleventy.js
@@ -9,4 +9,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addFilter("cssmin", function(code) {
     return new CleanCSS({}).minify(code).styles;
   });
+  eleventyConfig.addFilter("excerpt", function(content) {
+    return (content || '').substring(0, 200)
+  });
 };
