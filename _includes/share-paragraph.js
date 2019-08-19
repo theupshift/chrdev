140e5f9ed7de0a89fdd735d9a6e935adcefa22f5
Christian Fei <crifei93@gmail.com>
2019-08-19 23:47:26 +0200

    share paragraph

---
+++ _includes/share-paragraph.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+  const paragraphs = document.querySelectorAll('main p')
+  console.log('paragraphs', paragraphs)
+  paragraphs.forEach(p => {
+    const el = document.createElement('a')
+    const url = encodeURIComponent(window.location.href)
+    const text = encodeURIComponent(`"${p.innerText}"`)
+    el.setAttribute('href', `https://twitter.com/intent/tweet?original_referer=${url}&ref_src=twsrc%5Etfw&text=${text}&tw_p=tweetbutton&url=${url}`)
+    el.classList.add('share-paragraph')
+    el.innerText = `Share “`
+    p.appendChild(el)
+    p.classList.add('share-paragraph-hover')
+    p.style = `
+    `
+  })
+}
