02e4585793745f64b1192f45b1974715fdddf83e
Christian Fei <crifei93@gmail.com>
2019-08-20 19:32:47 +0200

    Revert "remove sw"
    This reverts commit fa844c6fb00ed5ce04362dbe33048799886cafcb.

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const CleanCSS = require("clean-css");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("sw.preload.js");
   eleventyConfig.addPassthroughCopy("robots.txt");
   eleventyConfig.addCollection("posts", (collection) => {
     return collection.getFilteredByTag('post').reverse();
