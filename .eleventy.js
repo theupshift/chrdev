fa844c6fb00ed5ce04362dbe33048799886cafcb
Christian Fei <crifei93@gmail.com>
2019-08-20 19:17:30 +0200

    remove sw

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,6 @@ const CleanCSS = require("clean-css");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
-  eleventyConfig.addPassthroughCopy("sw.preload.js");
   eleventyConfig.addPassthroughCopy("robots.txt");
   eleventyConfig.addCollection("posts", (collection) => {
     return collection.getFilteredByTag('post').reverse();
