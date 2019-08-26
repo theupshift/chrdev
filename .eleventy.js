7fcd46f9b428036ea8c418e4a4b4cda061ac7eab
Christian Fei <crifei93@gmail.com>
2019-08-26 07:35:49 +0200

    minify html

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const CleanCSS = require("clean-css");
+const htmlmin = require("html-minifier");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
@@ -9,6 +10,18 @@ module.exports = function(eleventyConfig) {
   });
   eleventyConfig.addFilter("cssmin", function(code) {
     return new CleanCSS({}).minify(code).styles;
+  });
+  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+    if( outputPath.endsWith(".html") ) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true
+      });
+      return minified;
+    }
+
+    return content;
   });
   eleventyConfig.addFilter("excerpt", function(content) {
     return (content || '').substring(0, 200)
