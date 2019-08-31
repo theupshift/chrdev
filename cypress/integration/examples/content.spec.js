8415eb6803d593c4bbf0e0634c0ba81f5ef43e12
Christian Fei <crifei93@gmail.com>
2019-08-31 12:31:31 +0200

    content spec

---
+++ cypress/integration/examples/content.spec.js
@@ -0,0 +1,22 @@
+/// <reference types="Cypress" />
+
+context('Content', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:8080')
+  })
+
+  it('posts', () => {
+    cy.get('body > header').contains('posts').click()
+
+    cy.location('pathname').should('include', 'posts')
+
+    cy.get('body').get('.post-link')
+  })
+  it('about', () => {
+    cy.get('body > header').contains('about me').click()
+
+    cy.location('pathname').should('include', 'about')
+
+    cy.get('body').contains('coding as a passion')
+  })
+})
