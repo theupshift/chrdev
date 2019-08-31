650cc25bf7c8dd15c7a0c54c7e1ada688b75131c
Christian Fei <crifei93@gmail.com>
2019-08-31 08:58:42 +0200

    cypress

---
+++ cypress/integration/examples/navigation.spec.js
@@ -0,0 +1,31 @@
+/// <reference types="Cypress" />
+
+context('Navigation', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:8080')
+  })
+
+  it('about', () => {
+    cy.get('body > header').contains('about me').click()
+
+    cy.location('pathname').should('include', 'about')
+  })
+  it('posts', () => {
+    cy.get('body > header').contains('posts').click()
+
+    cy.location('pathname').should('include', 'posts')
+  })
+  context('links', () => {
+    it('work', () => {
+      cy.get('body > header').contains('pomodoro.cc')
+      cy.get('body > header').contains('wonderflow')
+    })
+    it('social', () => {
+      cy.get('body > header').contains('twitter')
+      cy.get('body > header').contains('linkedin')
+    })
+    it('edit page', () => {
+      cy.get('body > header').contains('Edit this page on GitHub')
+    })
+  })
+})
