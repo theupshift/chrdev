660e92dfe6221eb618a30e16b518edb4a4d06696
Christian Fei <crifei93@gmail.com>
2019-12-11 21:38:31 +0100

    exclude drafts

--- .eleventy.js
+++ .eleventy.js
@@ -9,12 +9,16 @@ const changes = require('child_process').execSync(`git log -1 --no-color`).toStr
   .join('\n')
 const commit = require('child_process').execSync(`git rev-parse HEAD`).toString().trim()
 
+function reversed (tag, collection) {
+  return collection.getFilteredByTag(tag).filter(p => p.data && Array.isArray(p.data.tags) && !p.data.tags.includes('draft')).reverse()
+}
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets")
   eleventyConfig.addPassthroughCopy("robots.txt")
-  eleventyConfig.addCollection("posts", (collection) => collection.getFilteredByTag('post').reverse())
-  eleventyConfig.addCollection("last10posts", (collection) => collection.getFilteredByTag('post').reverse().slice(0, 10))
-  eleventyConfig.addCollection("featured", (collection) => collection.getFilteredByTag('featured').reverse().slice(0, 5))
+  eleventyConfig.addCollection("posts", (collection) => reversed('post', collection))
+  eleventyConfig.addCollection("last10posts", (collection) => reversed('post', collection).slice(0, 10))
+  eleventyConfig.addCollection("featured", (collection) => reversed('featured', collection).slice(0, 5))
   eleventyConfig.addTransform("htmlmin", (content, outputPath) => {
     if (!outputPath.endsWith(".html")) return content
     return htmlmin.minify(content, {
--- _drafts/2019-12-10-test-driven-brainfuck-compiler.md
+++ _drafts/2019-12-10-test-driven-brainfuck-compiler.md
@@ -4,4 +4,5 @@ date: 2019-12-10
 layout: post.njk
 tags:
   - post
+  - draft
 ---
