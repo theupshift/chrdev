eae8c59f099326a358d9c4fbcd6bbbcbb4f7f067
Christian Fei <crifei93@gmail.com>
2020-01-26 23:57:26 +0100

    spec

--- cypress/integration/content.spec.js
+++ cypress/integration/content.spec.js
@@ -1,24 +1,27 @@
 /// <reference types="Cypress" />
 
 context('Content', () => {
-  beforeEach(() => {
-    cy.visit('http://localhost:8080')
-  })
-
   it('about', () => {
+    cy.visit('http://localhost:8080')
+
     cy.get('body > header').contains('about').click()
 
     cy.location('pathname').should('include', 'about')
   })
 
   it('visible featured posts', () => {
+    cy.visit('http://localhost:8080')
+
     cy.get('body').contains('Featured blog posts')
   })
 
   describe('single post', () => {
-    beforeEach(() => {
-      cy.get('.post-item a').eq(0).click()
+    before(() => {
+      cy.visit('http://localhost:8080')
+
+      cy.get('.featured-post').eq(0).click()
     })
+
     it('navigates to /posts/{post-slug}', () => {
       cy.location('pathname').should('include', 'posts')
     })
