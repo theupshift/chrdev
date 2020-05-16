8cd5ead3ba813055dff6ccf971c8254e6a664c1d
Christian Fei <crifei93@gmail.com>
2020-05-16 15:35:32 +0200

    improve lazy

--- _includes/js/main.js
+++ _includes/js/main.js
@@ -87,7 +87,7 @@ function lazyLoad (selector = '[lazy]') {
       rect.top >= 0 &&
       rect.left >= 0 &&
       rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth) + rect.width
     )
   }
 }
