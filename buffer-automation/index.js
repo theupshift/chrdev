a9e94f74c9d33462e7d8c78751cd9a671b1d9e2e
Christian Fei <crifei93@gmail.com>
2020-07-22 22:15:03 +0200

    improve RANDOM and DRYRUN

--- buffer-automation/index.js
+++ buffer-automation/index.js
@@ -5,34 +5,47 @@ const addToQueue = require('./add-to-queue')
 const login = require('./login')
 
 ;(async () => {
+  const siteContent = fs.readFileSync(path.resolve(process.cwd(), 'site.json'))
+  const site = JSON.parse(siteContent)
+
+  const files = site.files.filter(file => file.attributes && Array.isArray(file.attributes.tags) && file.attributes.tags.includes('post'))
+  const notFeatured = files.filter(file => !file.attributes.tags.includes('featured'))
+  const featured = files.filter(file => file.attributes.tags.includes('featured'))
+  const toShare = []
+  if (process.env.RANDOM === 'true') {
+    toShare.push(...notFeatured.reduce((acc, file, i) => {
+      const attr = file.attributes
+      if (Math.random() < 0.5 && attr.tags.includes('post')) {
+        return acc.concat([`"${attr.title}", by @christian_fei https://cri.dev${file.url} `])
+      }
+      return acc
+    }, []))
+  } else {
+    toShare.push(...featured.reduce((acc, file, i) => {
+      const attr = file.attributes
+      if (process.env.RANDOM === 'true') {
+      } else {
+        if (attr.tags.includes('featured')) {
+          const tagsString = attr.tags.filter(t => !['post', 'featured', 'general'].includes(t)).map(t => `#${t}`).join(' ')
+          return acc.concat([`"${attr.title}", by @christian_fei https://cri.dev${file.url} ${tagsString} `])
+        }
+      }
+      return acc
+    }, []).reverse()
+    )
+  }
+  toShare.length = 10
+  console.log('toShare', toShare.length, toShare)
+
+  if (process.env.DRYRUN === 'true') {
+    return
+  }
+
   console.log('creating browser')
   const browser = await puppeteer.launch({ headless: process.env.HEADLESS !== 'false', timeout: 0 })
   console.log('creating page')
   const page = await browser.newPage()
   await page.setViewport({ width: 1280, height: 800 })
-
-  const siteContent = fs.readFileSync(path.resolve(process.cwd(), 'site.json'))
-  const site = JSON.parse(siteContent)
-
-  const files = site.files.filter(file => file.attributes && Array.isArray(file.attributes.tags) && file.attributes.tags.includes('post'))
-
-  const toShare = files.reduce((acc, file, i) => {
-    const attr = file.attributes
-    if (process.env.RANDOM === 'true') {
-      if (Math.random() < 0.5 && attr.tags.includes('post')) {
-        return acc.concat([`"${attr.title}", by @christian_fei https://cri.dev${file.url} `])
-      }
-    } else {
-      if (attr.tags.includes('featured')) {
-        const tagsString = attr.tags.filter(t => !['post', 'featured', 'general'].includes(t)).map(t => `#${t}`).join(' ')
-        return acc.concat([`"${attr.title}", by @christian_fei https://cri.dev${file.url} ${tagsString} `])
-      }
-    }
-    return acc
-  }, []).reverse()
-  toShare.length = 10
-  // toShare = toShare.slice(10, 20)
-  console.log('toShare', toShare.length, toShare)
 
   await login(page)
 
